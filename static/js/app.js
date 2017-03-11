fd8a26ba63a65d83fbfd6a27e9ac911e893328f4
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-03-11 13:44:22 +0100

    add timer

--- static/js/app.js
+++ static/js/app.js
@@ -119,6 +119,26 @@
             }
         });
 
+
+        // Timer
+        var timer2 = "15:00";
+        var interval = setInterval(function () {
+
+
+            var timer = timer2.split(':');
+            //by parsing integer, I avoid all extra string processing
+            var minutes = parseInt(timer[0], 10);
+            var seconds = parseInt(timer[1], 10);
+            --seconds;
+            minutes = (seconds < 0) ? --minutes : minutes;
+            if (minutes < 0) clearInterval(interval);
+            seconds = (seconds < 0) ? 59 : seconds;
+            seconds = (seconds < 10) ? '0' + seconds : seconds;
+            //minutes = (minutes < 10) ?  minutes : minutes;
+            $('#countdown').html(minutes + ':' + seconds);
+            timer2 = minutes + ':' + seconds;
+        }, 1000);
+
         $('#myselect').change(function () {
             editor.getSession().setMode("ace/mode/" + $('#myselect :selected').val());
         })
